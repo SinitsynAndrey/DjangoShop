e8afc6bdc91dab966548ba15851d50be7b07f62a
Андрей Синицын <andrey-sin@yandex.ru>
2022-07-27 12:51:41 +0300

    delete pycache and modify gitignore

--- basketapp/views.py
+++ basketapp/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.decorators import login_required
 from django.template.loader import render_to_string
 from django.http import JsonResponse
 
+
 @login_required
 def basket(request):
     title = 'корзина'
@@ -17,6 +18,7 @@ def basket(request):
     }
     return render(request, 'basketapp/basket.html', content)
 
+
 @login_required
 def basket_add(request, pk):
     if 'login' in request.META.get('HTTP_REFERER'):
@@ -24,15 +26,16 @@ def basket_add(request, pk):
 
     product = get_object_or_404(Products, pk=pk)
 
-    basket = Basket.objects.filter(user= request.user, product=product).first()
+    basket = Basket.objects.filter(user=request.user, product=product).first()
 
     if not basket:
-        basket = Basket(user= request.user, product= product)
+        basket = Basket(user=request.user, product=product)
 
     basket.quantity += 1
     basket.save()
 
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
 
 @login_required
 def basket_remove(request, pk):
@@ -41,25 +44,26 @@ def basket_remove(request, pk):
 
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
+
 @login_required
 def basket_edit(request, pk, quantity):
-    if request.is_ajax():
-        quantity = int(quantity)
-        new_basket_item = Basket.objects.get(pk=int(pk))
+    quantity = int(quantity)
+    new_basket_item = Basket.objects.get(pk=int(pk))
 
-        if quantity > 0:
-            new_basket_item.quantity = quantity
-            new_basket_item.save()
-        else:
-            new_basket_item.delete()
+    if quantity > 0:
+        new_basket_item.quantity = quantity
+        new_basket_item.save()
+    else:
+        new_basket_item.delete()
 
-        basket_items = Basket.objects.filter(user=request.user).order_by('created_at')
+    basket_items = Basket.objects.filter(user=request.user).order_by('created_at')
 
-        content = {
-            'basket_items': basket_items
-        }
+    content = {
+        'basket_items': basket_items
+    }
 
-        result = render_to_string('basketapp/includes/inc_basket_list.html', content)
+    result = render_to_string('basketapp/includes/inc_basket_list.html', content)
 
-        return JsonResponse({'result': result})
+    print(result)
 
+    return JsonResponse({'result': result})
